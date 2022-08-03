c9fb9139a0be5935078d8449320141ff8d9c8ad5
blackcatunderrain <a27v@outlook.com>
2022-08-03 12:07:50 +0300

    add search and some fixes

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, request
 from posts.comment import Comment
 from posts.comment_dao import CommentDAO
 from posts.post import Post
@@ -22,7 +22,25 @@ def page_posts_index():
 def page_posts_single(pk: int):
     post: Post | None = post_dao.get_by_pk(pk)
     comments: list[Comment] = comment_dao.get_comments_by_post_pk(pk)
-
     if post is None:
         abort(404)
     return render_template("post.html", post=post, comments=comments)
+
+
+@posts.route("/users/<user_name>")
+def page_post_by_user(user_name):
+    posts_: list[Post] = post_dao.get_by_poster(user_name)
+    return render_template("user-feed.html", user_name=user_name, posts=posts_)
+
+
+@posts.route("/search/")
+def page_post_search():
+    query: str = request.args.get("s", "")
+    if query == "":
+        posts_ = []
+    else:
+        posts_ = post_dao.search_by_word(query)
+    return render_template("search.html", posts=posts_, query=query)
+
+
+
